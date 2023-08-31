99232e7619c14ea0119c10663f9a94227f408f10
Alvin Shiem Dingcong <alvinshiemd@gmail.com>
2023-08-31 20:48:49 +0800

    conditional element

---
+++ 17_javascript_conditionals/main.js
@@ -0,0 +1,54 @@
+// java script conditional
+let grade = parseInt(prompt("What is your grade?"));
+
+if (grade < 75) {
+  console.log("failed! Try again next quarter.");
+} else if (grade <= 80) {
+  console.log("pass! Exert more effort");
+} else if (grade <= 85) {
+  console.log("Good! keep it up");
+} else if (grade <= 90) {
+  console.log("Very good. A job well done!");
+} else {
+  console.log("Execellent!");
+}
+
+/*
+if (grade > 80) {
+  console.log("failed! Try again next quarter.");
+} else {
+  console.log("pass! Exert more effort");
+}
+
+if (grade < 85) {
+  console.log("Good! keep it up");
+}
+
+if (grade < 90) {
+  console.log("Very good. A job well done!");
+}
+
+if (grade < 95) {
+  console.log("Excellent!");
+}
+
+/*
+let userLoggedIn = false;
+
+if (userLoggedIn) {
+  console.log("Go to Homepage");
+} else {
+  console.log("Redirect to login page");
+}*/
+
+/*
+let hour = 15;
+
+if (hour < 12) {
+  console.log("Good morning");
+} else if (hour < 18) {
+  console.log("Good Afternoon");
+} else {
+  console.log("Good Evening");
+}
+*/
