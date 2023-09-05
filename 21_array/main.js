1629968f12e8e333f72e8690f5bbafacbf77c024
Alvin Shiem Dingcong <alvinshiemd@gmail.com>
2023-09-05 20:51:56 +0800

    add array condition

---
+++ 21_array/main.js
@@ -0,0 +1,36 @@
+// array type of string
+// let fruits = ["Apple", "Banana", "Orange"];
+
+// console.log(fruits[1]);
+// let cars = ["ferrari", "lambo", "ford", "hundai", "mitsubishi"];
+//cars.unshift("suzuki"); //adding at the beginning of the array
+//cars.push("kia"); //adding items in the end of the array;
+//cars[1] = "lambogini";
+// console.log(cars);
+// let size = cars.length; // amount of the array
+//cars.splice(1, 0); // specific remove the array elements
+//cars.shift(); //remove the first element of array
+// cars.pop(); //remove the last element of array
+//
+// const vegetables = ["tomato", "potato", "onion", "Cucumber"];
+// const meats = ["chicken", "pork", "beef", "lamb"];
+// const food = cars.concat(vegetables);
+// console.log(food);
+
+const fruits = ["apple", "banana", "orange"];
+
+// for (initialization; condition; iteration ) {}
+// for (let i = 0; i < fruits.length; i++) {
+// console.log(fruits[i]);
+// }
+// for each is the same at the for loop
+//fruits.forEach(function (fruit) {
+// console.log(fruit);
+// });
+// for (let fruit of fruits) {
+// console.log(fruit);
+// }
+while (i < fruits.length) {
+  console.log(fruits);
+  i++;
+}
