2a92a971687dabc1ef0dac7f828c68c20ff20508
Alvin Shiem Dingcong <alvinshiemd@gmail.com>
2023-09-07 20:36:52 +0800

    add error and clean

---
+++ 23_errors_and_clean_code/main.js
@@ -0,0 +1,58 @@
+// Errors and clean Code
+//const a = "hello";
+//const b = "World";
+
+//console.log(c);
+
+// syntax Error
+// function printhelloWorld() {
+// console.log"Hello, World!";
+// }
+
+// Type Error
+// an operand or argument passed to a function is not of the expected type by that operator or function
+// or when attemting to modify a value that cannot be changed
+// or when attempting to use a value in an inappropriate way.
+
+// function add(x, y) {
+// return x + y;
+// }
+// console.log(add(1, 2));
+
+// reference error
+// const str1 = "Hello";
+// const str2 = "World!";
+// const message = str1.push(str2);
+
+// const name = "alvin";
+// name = "alvins";
+
+// clean Code
+// Naming fuction and variables
+// meaningful
+// descriptive
+// use a consistent vocabulary or naming convention
+
+function getPlayeScore() {}
+function fetchPlayerName() {}
+function retrievePlayer1Tag() {}
+function getPlayeScore() {}
+function getPlayerName() {}
+function getPlayer1Tag() {}
+/* const x = function(z){
+  let w = 0;z.forEach(
+    function (q){
+      w += q;
+
+});return w ;
+};
+x([2,2,2]);
+
+
+const sumArray = (array)  {
+  let sum = 0;
+
+  array.forEach((number) => sum += number);
+  return sum;
+}
+sumArray([2,2,2]);*/
