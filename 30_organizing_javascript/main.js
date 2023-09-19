6b1f49c473e82934bfed1293e8952bcc4ec82846
Alvin Shiem Dingcong <alvinshiemd@gmail.com>
2023-09-19 20:45:05 +0800

    add new session

--- 30_organizing_javascript/main.js
+++ 30_organizing_javascript/main.js
@@ -1,3 +1,45 @@
 // organizing javascript
 
-const fruits = ["apple", "orange", "banana"];
+// 1. plain old javascript objects
+
+// 2. object contructors
+// 3. Factory functions
+// 4. Module pattern
+// 5. Classes
+// 6. ES6 Modules
+
+function pokemonFactory(name, level) {
+  let health = level * 2;
+
+  const getLevel = () => level;
+  const getName = () => name;
+
+  const die = () => console.log(`${name} has reached zero hp. I'm dead!`);
+
+  const damage = (x) => {
+    health -= x;
+
+    if (health <= 0) die();
+  };
+
+  const attack = (enemy) => {
+    if (level < enemy.getLevel()) {
+      damage(1);
+      console.log(`${enemy.getName()} has damaged ${name}`);
+    }
+
+    if (level >= enemy.getLevel()) {
+      enemy.damage(5);
+      console.log(`${name} has damaged ${enemy.getName()}.`);
+    }
+  };
+
+  return { attack, damage, getLevel, getName };
+}
+
+const ashPlayer = pokemonFactory("pikachu", 10);
+const computerPlayer = pokemonFactory("ratata", 5);
+
+ashPlayer.attack(computerPlayer);
+computerPlayer.attack(ashPlayer);
+ashPlayer.attack(computerPlayer);
